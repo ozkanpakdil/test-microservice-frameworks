8996eb1fce0735d4daa76a22ee6dcad702bde2d8
ozkanpakdil <ozkan.pakdil@gmail.com>
2020-08-25 09:34:56 +0200

    cleanup and gh action fix

--- runGatling.sh
+++ runGatling.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 # set -x
-echo 'Running jars and collecting test results...'
 
 >test-result.md
+
+
+DATE=$(date +"%Y-%m-%d %T")
+echo "---
+layout: post
+title:  \"Getting com.fasterxml.jackson.databind.exc.InvalidDefinitionException: No serializer found for class\"
+date:   $DATE
+categories: java,fasterxml,json
+--- 
+Here is total package generation times for seperate modules,
+{% highlight bash %}
+" >> test-result.md
+mvn -T 1C test package|grep SUCCESS|grep s >>test-result.md
+echo '{% endhighlight %}' >> test-result.md
+echo 'Size of created packages: 
+| Size in MB |  Name |
+|----------|:------:|' >> test-result.md
+ls -lh */target/*.jar|grep M|awk '{print "|",$5,"|",$9,"|"}' >>test-result.md
+printf '\n' >> test-result.md
+echo 'Running jars and collecting test results...'
 
 test (){
     jarPath=$1
@@ -18,11 +37,11 @@ test (){
     frameworkVersion=`grep --color=never -o "$verInfo.*" log.log`
     startTime=`grep --color=never -o "$startTime.*" log.log`
 
-    echo "# " $frameworkVersion $startTime >> test-result.md
+    echo $frameworkVersion $startTime >> test-result.md
     printf "\nGatling test starting... for $jarPath"
-    echo '```' >> test-result.md
+    echo '{% highlight bash %}' >> test-result.md
     mvn -f gatling/pom.xml gatling:test|grep -A10 "Global Information" >> test-result.md
-    echo '```' >> test-result.md
+    echo '{% endhighlight %}' >> test-result.md
     ps -ef|grep java|awk '{print $2}'|xargs kill
     printf '\n' >> test-result.md
     
@@ -31,7 +50,7 @@ test (){
 test "spring-boot/target/springboot-demo-0.0.1-SNAPSHOT.jar" ":: Spring Boot ::" "Started DemoApplication"
 test "quarkus/target/quarkus-demo-1.0.0-SNAPSHOT-runner.jar" "powered by Quarkus" "WWWWW"
 test "micronaut/target/micronaut-demo-0.1.jar" "micronaut version" "Startup completed in"
-test "helidon-se/target/helidon-quickstart-se.jar" "Helidon SE" "XXXXX"
+# test "helidon-se/target/helidon-quickstart-se.jar" "Helidon SE" "XXXXX"
 # test "eclipse-microprofile/target/eclipse-microprofile-demo-1.0-SNAPSHOT.jar" "Open Liberty" "The defaultServer server started"
 
 
