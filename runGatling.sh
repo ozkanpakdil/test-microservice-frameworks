cc7e410871f2348680cd6e82064f2884697c6dde
ozkanpakdil <ozkan.pakdil@gmail.com>
2020-09-10 21:13:47 +0200

    fixing kumuluz report

--- runGatling.sh
+++ runGatling.sh
@@ -43,8 +43,8 @@ test (){
         sleep 0.4
     done
 
-    frameworkVersion=`grep -o "$verInfo.*" log.log`
-    startTime=`grep -o "$startTime.*" log.log`
+    frameworkVersion=`grep -m1 -o "$verInfo.*" log.log`
+    startTime=`grep -m1 -o "$startTime.*" log.log`
 
     echo $frameworkVersion $startTime >> test-result.md
     printf "\nGatling test starting... for $jarPath"
@@ -61,7 +61,7 @@ test "spring-boot/target/springboot-demo-0.0.1-SNAPSHOT.jar" ":: Spring Boot ::"
 test "quarkus/target/quarkus-demo-1.0.0-SNAPSHOT-runner.jar" "powered by Quarkus" "WWWWW"
 test "micronaut/target/micronaut-demo-0.1.jar" "micronaut version" "Startup completed in"
 test "vertx/target/vertx-demo-1.0.0-SNAPSHOT-fat.jar" "vertx version" "XXXXX"
-test "eclipse-microprofile-kumuluz-test/target/eclipse-microprofile-kumuluz-test.jar" "kumuluz version:" " Started"
+test "eclipse-microprofile-kumuluz-test/target/eclipse-microprofile-kumuluz-test.jar" "kumuluz version:" "Server -- Started"
 # test "helidon-se/target/helidon-quickstart-se.jar" "Helidon SE" "XXXXX"
 
 
