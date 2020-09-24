44155882c134c34c57dbd5eb8801e8b8f3b72bb2
ozkanpakdil <ozkan.pakdil@gmail.com>
2020-09-24 09:51:16 +0200

    clean up

--- eclipse-microprofile-kumuluz-test/src/main/java/com/mascix/eclipse/microprofile/kumuluz/test/HelloController.java
+++ eclipse-microprofile-kumuluz-test/src/main/java/com/mascix/eclipse/microprofile/kumuluz/test/HelloController.java
@@ -1,17 +1,12 @@
 package com.mascix.eclipse.microprofile.kumuluz.test;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.time.LocalDate;
 
-@Path("/hello")
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
-public class HelloController {
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 
+@Path("/hello")
+public class HelloController {
     @GET
     public ApplicationInfo sayHello() {
         return new ApplicationInfo("eclipse-microprofile-kumuluz", LocalDate.now().getYear());
