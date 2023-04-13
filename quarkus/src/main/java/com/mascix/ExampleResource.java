d041be9f26207ddce668f3c0bdc054e0de71f212
ozkanpakdil <ozkan.pakdil@gmail.com>
2023-04-13 19:52:20 +0100

    quarkus 3 changes

--- quarkus/src/main/java/com/mascix/ExampleResource.java
+++ quarkus/src/main/java/com/mascix/ExampleResource.java
@@ -2,15 +2,15 @@ package com.mascix;
 
 import java.time.LocalDate;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 
+import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/hello")
