8b95a7ecb213aef63dbb00a5bad088ec625814c4
ozkanpakdil <ozkan.pakdil@gmail.com>
2024-08-06 22:50:51 +0100

    ktor native image problem fixed

--- ktor-demo/src/Application.kt
+++ ktor-demo/src/Application.kt
@@ -3,7 +3,6 @@ package com.mascix
 import com.fasterxml.jackson.core.util.DefaultIndenter
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.SerializationFeature
-import io.ktor.client.engine.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
@@ -11,13 +10,12 @@ import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.util.*
 import java.time.LocalDate
 import java.util.*
 
 val props = Properties()
 fun main() {
-    props.load(ClassLoader.getSystemResourceAsStream("app.properties"))
+    props.load(ClassLoader.getSystemResourceAsStream("application.properties"))
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         mainModule()
     }.start(wait = true)
