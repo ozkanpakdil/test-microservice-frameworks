d655525372ded4bf0336e535f4e936df2e6de111
ozkanpakdil <ozkan.pakdil@gmail.com>
2025-08-02 20:29:53 +0100

    refactor: simplify configuration property loading with ApplicationContext

--- micronaut/src/main/java/com/mascix/Application.java
+++ micronaut/src/main/java/com/mascix/Application.java
@@ -1,24 +1,17 @@
 package com.mascix;
 
-import java.util.Properties;
-
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
 
 public class Application {
 
     public static void main(String[] args) {
-        try {
-            Properties prop = new Properties();
-            prop.load(Application.class.getClassLoader()
-                    .getResourceAsStream("application.yml"));
-
-            System.out.println("micronaut version:" + prop.getProperty("version"));
-        } catch (Exception e) {
-            System.err.println("Error loading properties: " + e.getMessage());
-            e.printStackTrace();
-        }
-
-
-        Micronaut.run(Application.class, args);
+        ApplicationContext context = Micronaut.run(Application.class, args);
+        
+        // Get the version from configuration
+        String version = context.getEnvironment()
+                .getProperty("micronaut.version", String.class)
+                .orElse("unknown");
+        System.out.println("micronaut version: " + version);
     }
 }
