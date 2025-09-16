4e76578b9fc04bff4bb0f240285de0166a5c9b97
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-09-16 08:49:47 -0400

    timer

--- avaje-jex-jdk/src/main/java/io/avaje/example/Main.java
+++ avaje-jex-jdk/src/main/java/io/avaje/example/Main.java
@@ -1,5 +1,7 @@
 package io.avaje.example;
 
+import java.lang.System.Logger;
+
 import io.avaje.inject.BeanScope;
 import io.avaje.jex.Jex;
 import io.avaje.jex.Routing.HttpService;
@@ -14,7 +16,7 @@ public final class Main {
    * @param args command line arguments.
    */
   public static void main(final String[] args) {
-
+    long start = System.currentTimeMillis();
     var routes = BeanScope.builder().build().list(HttpService.class);
     Jex.create()
         .routing(routes)
@@ -22,5 +24,8 @@ public final class Main {
         .jsonService(new JacksonJsonService())
         .get("/simple-greet", ctx -> ctx.text("Hello World!"))
         .start();
+    long end = System.currentTimeMillis();
+    System.getLogger("io.avaje.example.Main")
+        .log(Logger.Level.INFO, "Started in " + (end - start) + "ms");
   }
 }
