792e21c0694032df18d0729da19ff6493983ab21
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-09-15 20:55:07 -0400

    Update Main.java

--- avaje-jex-jdk/src/main/java/io/avaje/example/Main.java
+++ avaje-jex-jdk/src/main/java/io/avaje/example/Main.java
@@ -17,6 +17,7 @@ public final class Main {
 
     var routes = BeanScope.builder().build().list(HttpService.class);
     Jex.create()
+        .config(c -> c.host("localhost"))
         .routing(routes)
         //  .jsonService(new JsonbJsonService())
         .jsonService(new JacksonJsonService())
