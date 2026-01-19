6aa99b1019011374b2b4a169d29e1dfdbaaf3004
Jake Holland <contact@hollandjake.com>
2026-01-19 06:26:57 +0000

    feat(bun): improve bun performance
    - use bun native addons for routing and json serialization (resulting in p95 55µs)

--- bun/main.ts
+++ bun/main.ts
@@ -2,21 +2,15 @@ const port = 8080;
 
 const server = Bun.serve({
     port: port,
-    fetch(req) {
-        const url = new URL(req.url);
-        
-        if (url.pathname === "/hello") {
-            const info = {
+    routes: {
+        "/hello": () => {
+            return Response.json({
                 name: "bun",
                 releaseYear: new Date().getFullYear()
-            };
-            return new Response(JSON.stringify(info), {
-                headers: { "Content-Type": "application/json" }
-            });
-        }
-        
-        return new Response("Not Found", { status: 404 });
-    }
+            })
+        },
+        "/*": new Response("Not Found", { status: 404 })
+    },
 });
 
 console.log(`Bun server started on port ${server.port}`);
